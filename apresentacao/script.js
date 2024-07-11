ff565c8b44bb1d0aabeead2478d548582341d585
Joao Ricardo <ricardojoao412@gmail.com>
2024-07-11 09:10:05 -0400

    inicio do carousel, mudança de estilo das cores e nova pasta de imagens

--- apresentacao/script.js
+++ apresentacao/script.js
@@ -1,30 +1,34 @@
-// $(document).ready(function() {
-//     $(document).on('click', '#BOTAO', function(e) {
-//       alert('sem essa opção no momento :(');
-//       // Aqui você pode adicionar outras ações que deseja realizar
-//     });
-//   });
-//           var textoDigitado = "Jovem desenvolvedor, iniciando meus trabalhos com JavaScript, HTML, PHP e banco de dados.";
-//           var descricaoElemento = document.getElementById('descricaoDigitada');
-  
-//           function digitarTexto() {
-//               var textoTemporario = textoDigitado;
-//               var idx = 0;
-  
-//               var intervalo = setInterval(function () {
-//                   if (idx < textoTemporario.length) {
-//                       descricaoElemento.textContent += textoTemporario.charAt(idx);
-//                       idx++;
-//                   } else {
-//                       clearInterval(intervalo);
-//                   }
-//               }, 90); 
-//           }
 
-//           window.onload = function () {
-//               digitarTexto();
-//           };
-  
-//           $(function () {
-//               $('[data-toggle="tooltip"]').tooltip();
-//           });
+
+// $(document).ready(function() {
+// $(document).on('click', '#BOTAO', function(e) {
+//  alert('sem essa opção no momento :(');
+//  // Aqui você pode adicionar outras ações que deseja realizar
+// });
+// });
+
+
+//      var textoDigitado = "Jovem desenvolvedor, iniciando meus trabalhos com JavaScript, HTML, PHP e banco de dados.";
+//      var descricaoElemento = document.getElementById('descricaoDigitada');
+
+//      function digitarTexto() {
+//          var textoTemporario = textoDigitado;
+//          var idx = 0;
+
+//          var intervalo = setInterval(function () {
+//              if (idx < textoTemporario.length) {
+//                  descricaoElemento.textContent += textoTemporario.charAt(idx);
+//                  idx++;
+//              } else {
+//                  clearInterval(intervalo);
+//              }
+//          }, 80); // Velocidade da digitação (em milissegundos)
+//      }
+
+//      window.onload = function () {
+//          digitarTexto();
+//      };
+
+//      $(function () {
+//          $('[data-toggle="tooltip"]').tooltip();
+//      });
